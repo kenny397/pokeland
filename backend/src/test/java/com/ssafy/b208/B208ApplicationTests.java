0b8d0b6ae652ea32271c6e3d05400bdfebc52e01
kenny <kenny397@naver.com>
2022-03-10 13:15:59 +0900

    initial commit

---
+++ backend/src/test/java/com/ssafy/b208/B208ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ssafy.b208;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class B208ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
