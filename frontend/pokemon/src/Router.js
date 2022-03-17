ce1a42a219d574fd0c24221feb79f6a138e3633f
Leo <guswhddlf11@gmail.com>
2022-03-17 16:53:33 +0900

    build: router.js 초기세팅

---
+++ frontend/pokemon/src/Router.js
@@ -0,0 +1,17 @@
+import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
+const Router = () => {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" />
+        <Route path="/" />
+        <Route path="/" />
+        <Route path="/" />
+      </Routes>
+    </BrowserRouter>
+  );
+};
+
+export default Router;
