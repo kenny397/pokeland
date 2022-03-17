9c2991d9d0ceea8a8f7a8afea93462ddf09ed8ba
kenny <kenny397@naver.com>
2022-03-17 16:50:23 +0900

    feat : 서비스 추가

---
+++ backend/src/main/java/com/ssafy/b208/api/service/GachaService.java
@@ -0,0 +1,5 @@
+package com.ssafy.b208.api.service;
+
+public interface GachaService {
+
+}
---
+++ backend/src/main/java/com/ssafy/b208/api/service/GachaServiceImpl.java
@@ -0,0 +1,8 @@
+package com.ssafy.b208.api.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class GachaServiceImpl implements GachaService {
+
+}
