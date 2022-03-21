8c616afe18662a6651151aa0cc80e027047aee60
Leo <guswhddlf11@gmail.com>
2022-03-21 22:17:43 +0900

    chore: redux 초기코드 작성 후 수정

---
+++ frontend/pokemon/src/pages/MainPage.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { Link } from 'react-router-dom';
+
+export default function MainPage() {
+  return (
+    <div>
+      <h1>
+        MainPage
+      </h1>
+      <Link to="/support">
+        <button>
+          Support page
+        </button>
+      </Link>
+    </div>
+  );
+}
