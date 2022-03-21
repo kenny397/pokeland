d9dc3da3471de8d09be9cd66c3e873cff1e6fd8f
onys <thkim2017@gmail.com>
2022-03-21 14:32:18 +0900

    feat: 🎸 도메인 주소로 들어왔을 때 Router가 판단 할 것 생각해서 path 만들기
    tutorial 완료, 지갑 연결 완료 시 /main

--- frontend/pokemon/src/Router.js
+++ frontend/pokemon/src/Router.js
@@ -2,12 +2,18 @@ import React from "react";
 import { useRoutes } from "react-router-dom";
 
 import IntroPage from './IntroPage';
-import SupportPage from './SupportPage';
+import MainPage from './MainPage';
+import TutorialPage from './TutorialPage';
+import SupportPage from './support/SupportPage';
+import NotFound from './NotFound';
 
 const Router = () => {
   let element = useRoutes([
     { path: "/", element: <IntroPage /> },
+    { path: "/main", element: <MainPage /> },
+    { path: "/tutorial", element: <TutorialPage /> },
     { path: "/support", element: <SupportPage /> },
+    { path: "*", element: <NotFound /> }
   ]);
 
   return element;
