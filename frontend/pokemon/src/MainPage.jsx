fd8f5f64dd13bf17fc5e2538c14756a85c06a027
onys <thkim2017@gmail.com>
2022-03-21 15:53:05 +0900

    feat: 🎸 고객센터 페이지와 main Page 연결

--- frontend/pokemon/src/MainPage.jsx
+++ frontend/pokemon/src/MainPage.jsx
@@ -1,9 +1,17 @@
 import React from "react";
+import { Link } from 'react-router-dom';
 
 export default function MainPage() {
   return (
     <div>
-      MainPage
+      <h1>
+        MainPage
+      </h1>
+      <Link to="/support">
+        <button>
+          Support page
+        </button>
+      </Link>
     </div>
   );
 }
