e181467b94e9ffffb43951f9ff9ac8d3040713de
onys <thkim2017@gmail.com>
2022-03-23 20:20:46 +0900

    feat: 🎸 한 페이지에 6마리씩 포켓몬이 나오게 구현
    useState 사용

--- frontend/pokemon/src/pokedex/PokedexPage.jsx
+++ frontend/pokemon/src/pokedex/PokedexPage.jsx
@@ -1,17 +1,31 @@
-import React from "react";
-import pokemonList from "../fixtures/pokemonList";
-import PokemonItem from "./PokemonItem";
+import React, { useState } from "react";
+import PokemonList from "./PokemonList";
 
 export default function PokedexPage() {
+  const [page, setPage] = useState(1);
+  
+  const onClickGoToPrev = () => {
+    setPage(page - 1);
+    if (page <= 1) {
+      setPage(1);
+    }
+  };
+
+  const onClickGoToNext = () => {
+    setPage(page + 1);
+    if (page >= 26) {
+      setPage(26);
+    }
+  };
+
   return (
     <>
       <h1>Pokedex</h1>
-      {pokemonList.map((item) => (
-        <PokemonItem
-          item={item}
-          key={item['id']}
-        />
-      ))}
+      <PokemonList 
+        page={page}
+      />
+      <button onClick={onClickGoToPrev}>왼쪽</button>
+      <button onClick={onClickGoToNext}>오른쪽</button>
     </>
   );
 }
--- frontend/pokemon/src/pokedex/PokemonItem.jsx
+++ frontend/pokemon/src/pokedex/PokemonItem.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-export default function PokemonItem({ item: { id, name } }) {
+export default function PokemonItem({ page, item: { id, name } }) {
   const str = "" + id;
   const pad = "000";
   const pokemonNum = pad.substring(0, pad.length - str.length) + str;
@@ -9,7 +9,8 @@ export default function PokemonItem({ item: { id, name } }) {
   return (
     <>
       <p>NO. {pokemonNum} {name}</p>
-      <img src={pokemonShadowImgPath} alt="pokemonImg" width="150px" />
+      <p>{page}</p>
+      <img src={pokemonShadowImgPath} alt="pokemonImg" width="100px" />
     </>
   );
 }
