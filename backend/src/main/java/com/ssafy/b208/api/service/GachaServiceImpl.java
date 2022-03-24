2408c0665895d9eb5b813de6c1d6a3c293b2a9a8
kenny <kenny397@naver.com>
2022-03-24 13:44:30 +0900

    fix : webj3삭제

--- backend/src/main/java/com/ssafy/b208/api/service/GachaServiceImpl.java
+++ backend/src/main/java/com/ssafy/b208/api/service/GachaServiceImpl.java
@@ -26,64 +26,17 @@ import java.util.Arrays;
 
 @Service
 public class GachaServiceImpl implements GachaService {
-    private String contract="0xe2798e7585BD0c48e82d9b1569bBa5aE092345a3";
-    private String pwd="test";
-    private Admin web3j=null;
 
-    public GachaServiceImpl(){
-        web3j = Admin.build(new HttpService("http://localhost:8545"));
-    }
+
+
 
     @Override
     public void getNft(WalletRequestDto wallet) throws Exception {
-        String from = wallet.getWalletId();
-        Function function = new Function("create",
-                Arrays.asList(new Address(from)),
-                Arrays.asList(new TypeReference<Uint256>() {}));
-        PersonalUnlockAccount personalUnlockAccount = web3j.personalUnlockAccount(from,pwd).send();
-        if (personalUnlockAccount.accountUnlocked()) { // unlock 일때
 
-            EthGetTransactionCount ethGetTransactionCount = web3j.ethGetTransactionCount(
-                    from, DefaultBlockParameterName.LATEST).sendAsync().get();
 
-            BigInteger nonce = ethGetTransactionCount.getTransactionCount();
-
-            //3. Transaction값 제작
-            Transaction transaction = Transaction.createFunctionCallTransaction(from, nonce,
-                    Transaction.DEFAULT_GAS,
-                    null, contract,
-                    FunctionEncoder.encode(function));
-
-            // 4. ethereum Call &
-            EthSendTransaction ethSendTransaction = web3j.ethSendTransaction(transaction).send();
-
-            // transaction에 대한 transaction Hash값 얻기.
-            String transactionHash = ethSendTransaction.getTransactionHash();
-            Thread.sleep(5000);
-            System.out.println("tra = " + transactionHash);
-            TransactionReceipt receipt = getReceipt(transactionHash);
-            System.out.println("receipt = " + receipt);
-            // System.out.println("log"+receipt.getLogs().get(0).getTopics().get(0));
-
-        }
 
     }
-    public TransactionReceipt getReceipt(String transactionHash) throws IOException {
-
-        EthGetTransactionReceipt transactionReceipt = web3j.ethGetTransactionReceipt(transactionHash).send();
-
-        if(transactionReceipt.getTransactionReceipt().isPresent())
-        {
-            System.out.println("transactionReceipt.getResult().getContractAddress() = " +
-                    transactionReceipt.getResult());
-        }
-        else
-        {
-            System.out.println("transaction complete not yet");
-        }
-
-        return transactionReceipt.getResult();
-    }
+   
 
 
 
