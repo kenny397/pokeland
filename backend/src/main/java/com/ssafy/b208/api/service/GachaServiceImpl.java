1279ebea56e42165407442cc1df12da93579e43a
kenny <kenny397@naver.com>
2022-03-24 15:21:28 +0900

    feat : 뽑기 서비스

--- backend/src/main/java/com/ssafy/b208/api/service/GachaServiceImpl.java
+++ backend/src/main/java/com/ssafy/b208/api/service/GachaServiceImpl.java
@@ -1,41 +1,18 @@
 package com.ssafy.b208.api.service;
 
-import com.ssafy.b208.api.dto.request.WalletRequestDto;
-import com.ssafy.b208.api.exception.AddressNotFoundException;
-import org.springframework.boot.autoconfigure.kafka.KafkaProperties;
+import com.ssafy.b208.api.db.repository.UserPokemonRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
-import org.web3j.abi.FunctionEncoder;
-import org.web3j.abi.TypeReference;
-import org.web3j.abi.datatypes.Address;
-import org.web3j.abi.datatypes.Function;
-import org.web3j.abi.datatypes.Utf8String;
-import org.web3j.abi.datatypes.generated.Uint256;
-import org.web3j.protocol.admin.Admin;
-import org.web3j.protocol.admin.methods.response.PersonalUnlockAccount;
-import org.web3j.protocol.core.DefaultBlockParameterName;
-import org.web3j.protocol.core.methods.request.Transaction;
-import org.web3j.protocol.core.methods.response.EthGetTransactionCount;
-import org.web3j.protocol.core.methods.response.EthGetTransactionReceipt;
-import org.web3j.protocol.core.methods.response.EthSendTransaction;
-import org.web3j.protocol.core.methods.response.TransactionReceipt;
-import org.web3j.protocol.http.HttpService;
-
-import java.io.IOException;
-import java.math.BigInteger;
-import java.util.Arrays;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
+@Transactional(readOnly = true)
+@RequiredArgsConstructor
 public class GachaServiceImpl implements GachaService {
+    private final UserPokemonRepository userPokemonRepository;
 
 
 
-
-    @Override
-    public void getNft(WalletRequestDto wallet) throws Exception {
-
-
-
-    }
 
 
 
