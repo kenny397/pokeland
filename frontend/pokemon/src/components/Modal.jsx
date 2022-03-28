75e9edc0c31e4adf8c027184b338963ce317fc28
Leo <guswhddlf11@gmail.com>
2022-03-28 18:27:16 +0900

    feat: 모달 x버튼 추가

--- frontend/pokemon/src/components/Modal.jsx
+++ frontend/pokemon/src/components/Modal.jsx
@@ -5,6 +5,7 @@ export default function Modal({ setIsVisible, InnerComponent }) {
 
   return (
     <div className="modal-container">
+      <p className="modal-close-handler" onClick={() => setIsVisible(false)}>X</p>
       <InnerComponent handleClickCloseModal={() => setIsVisible(false)}/>
     </div>
   );
