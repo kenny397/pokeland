d9b76e021b07c19e76b6a9752ed67ccd37da688b
Leo <guswhddlf11@gmail.com>
2022-03-28 20:37:01 +0900

    feat: 사이드바 버거버튼 내부 기능 수정

--- frontend/pokemon/src/mainpage/SideBar.jsx
+++ frontend/pokemon/src/mainpage/SideBar.jsx
@@ -1,24 +1,53 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { slide as Menu } from "react-burger-menu";
 import { Link } from "react-router-dom";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBars } from "@fortawesome/free-solid-svg-icons";
+
+import './SideBar.css';
+
 export default function SideBar() {
+
+  const [isOpen, setIsOpen] = useState(false);
+
+  const clicked = () => {
+    setIsOpen(false);
+  };
+  const overlay = document.getElementsByClassName('bm-overlay');
+
+  useEffect(() => {
+    overlay[0].addEventListener("click", clicked);
+
+  });
+
   return (
-    <Menu>
-      <Link to={'/pokedex'}>
-        포켓몬 도감
-      </Link>
+    <>
+      <Menu 
+        customBurgerIcon={ false } 
+        customCrossIcon={ false }
+        isOpen={isOpen}
+        disableOverlayClick
+        disableAutoFocus 
+      >
+        <p className="sidebar-cross-btn" onClick={() => setIsOpen(false)}>X</p>
+        <Link to={'/pokedex'} onClick={() => setIsOpen(false)}>
+          포켓몬 도감
+        </Link>
 
-      <Link to={'/gacha'}>
-        포켓몬 뽑기
-      </Link>
+        <Link to={'/gacha'} onClick={() => setIsOpen(false)}>
+          포켓몬 뽑기
+        </Link>
 
-      <Link to={'/support'}>
-        고객센터
-      </Link>
-
-      <Link to={'/main'}>
-        메인
-      </Link>
-    </Menu>
+        <Link to={'/support'} onClick={() => setIsOpen(false)}>
+          고객센터
+        </Link>
+        
+        <Link to={'/main'} onClick={() => setIsOpen(false)}>
+          메인
+        </Link>
+        
+      </Menu>
+      <FontAwesomeIcon icon={faBars} className="burger-btn" onClick={() => setIsOpen(true)}/>
+    </>
   );
 }
