41c787356ad01a556436837794863362102cbed1
DESKTOP-KVCQHCD\SSAFY <shaian2@naver.com>
2022-03-29 12:06:15 +0900

    chore: 불필요한 어노테이션 제거

--- backend/src/main/java/com/ssafy/b208/api/service/SupportServiceImpl.java
+++ backend/src/main/java/com/ssafy/b208/api/service/SupportServiceImpl.java
@@ -9,7 +9,6 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 @Service
-@Transactional(readOnly = true)
 @RequiredArgsConstructor
 public class SupportServiceImpl implements SupportService {
     @Autowired
