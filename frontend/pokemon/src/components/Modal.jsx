559451ade4fc921e2431f45f2ad4d0eef2faf961
Leo <guswhddlf11@gmail.com>
2022-03-29 15:00:53 +0900

    chore: 모달 내부 x버튼 아이콘으로 변경

--- frontend/pokemon/src/components/Modal.jsx
+++ frontend/pokemon/src/components/Modal.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import "./Modal.scss";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faXmark } from "@fortawesome/free-solid-svg-icons";
 
 export default function Modal({ setIsVisible, InnerComponent, width, height }) {
 
   return (
     <div className="modal-container" style={ { width: width+'', height: height+'' } }>
-      <p className="modal-close-handler" onClick={() => setIsVisible(false)}>X</p>
+      <FontAwesomeIcon icon={faXmark} className="modal-close-handler" onClick={() => setIsVisible(false)}/>
       <InnerComponent handleClickCloseModal={() => setIsVisible(false)}/>
     </div>
   );
