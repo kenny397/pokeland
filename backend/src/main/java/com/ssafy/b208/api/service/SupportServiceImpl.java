71be0ac6ac19f2437576c97132ac80178651da87
DESKTOP-KVCQHCD\SSAFY <shaian2@naver.com>
2022-03-29 12:00:24 +0900

    feat: 고객센터 메일 전송 기능 추가

---
+++ backend/src/main/java/com/ssafy/b208/api/service/SupportServiceImpl.java
@@ -0,0 +1,29 @@
+package com.ssafy.b208.api.service;
+
+import com.ssafy.b208.api.dto.MailDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Transactional(readOnly = true)
+@RequiredArgsConstructor
+public class SupportServiceImpl implements SupportService {
+    @Autowired
+    private JavaMailSender mailSender;
+    private static final String FROM_ADDRESS = "YOUR_EMAIL_ADDRESS";
+
+    @Override
+    public void mailSend(MailDto mailDto) {
+        SimpleMailMessage message = new SimpleMailMessage();
+        String title = "[" + mailDto.getCategory() + "] " + mailDto.getTitle();
+        message.setTo(mailDto.getAddress());
+        message.setFrom(SupportServiceImpl.FROM_ADDRESS);
+        message.setSubject(title);
+        message.setText(mailDto.getMessage());
+        mailSender.send(message);
+    }
+}
