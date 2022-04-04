31968c8a289ee8d370fd6c7a56991441e722a3f6
DESKTOP-KVCQHCD\SSAFY <shaian2@naver.com>
2022-04-04 16:21:21 +0900

    feat: @ApiOperation, @ApiResponses 추가

--- backend/src/main/java/com/ssafy/b208/api/controller/PokedexController.java
+++ backend/src/main/java/com/ssafy/b208/api/controller/PokedexController.java
@@ -6,8 +6,7 @@ import com.ssafy.b208.api.db.entity.UserPokemon;
 import com.ssafy.b208.api.dto.request.UserRequestDto;
 import com.ssafy.b208.api.dto.response.*;
 import com.ssafy.b208.api.service.PokedexService;
-import io.swagger.annotations.Api;
-import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.*;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -35,6 +34,11 @@ public class PokedexController {
     // 원래 Request는 header에 jwtToken
     @GetMapping("/pokedex")
     @ApiOperation(value = "도감 조회", notes = "응답으로 사용자가 가진 포켓몬의 pokedex_id들이 담긴 리스트가 나온다. jwt토큰 필요")
+    @ApiResponses({
+            @ApiResponse(code = 200, message = "성공"),
+            @ApiResponse(code = 401, message = "실패 아직 구현 x"),
+            @ApiResponse(code = 500, message = "서버 오류")
+    })
     public ResponseEntity<PokemonListDto> getPokemonList(@ApiIgnore Authentication authentication) throws Exception {
 
         NftUserDetail nftUserDetail = (NftUserDetail)authentication.getDetails();
@@ -50,6 +54,7 @@ public class PokedexController {
     // 포켓몬 1마리 상세조회(도감 정보)
     @GetMapping("/pokedex/{pokedexId}")
     @ApiOperation(value = "포켓몬 도감 정보 상세 조회", notes = "pokedex id로 특정 포켓몬의 상세정보를 조회한다.")
+    @ApiImplicitParam(name = "pokedexId", value = "포켓몬 번호", required = true)
     public ResponseEntity<PokeInfoOuterDto> getPokeInfo(@PathVariable Long pokedexId) throws Exception {
 
         PokeDex pokeDex = pokedexService.getPokeInfo(pokedexId);
@@ -72,6 +77,10 @@ public class PokedexController {
     // 보유 NFP 리스트 by 한 포켓몬 조회
     @GetMapping("/nfp/{publicKey}/{pokedexId}")
     @ApiOperation(value = "특정 포켓몬의 보유 NFP 리스트 조회", notes = "public key와 pokedex id로 유저가 가진 특정 포켓몬의 NFP들을 조회한다.")
+    @ApiImplicitParams({
+        @ApiImplicitParam(name = "pokedexId", value = "포켓몬 번호", required = true),
+        @ApiImplicitParam(name = "publicKey", value = "공개키", required = true)
+    })
     public ResponseEntity<NfpListDto> getNfpList(@PathVariable String publicKey, @PathVariable Long pokedexId) throws Exception {
 
         List<NfpDetailDto> nfpList = pokedexService.getNfpList(publicKey, pokedexId);
@@ -85,6 +94,7 @@ public class PokedexController {
     //jwt
     @GetMapping("/nfp/detail/{tokenId}")
     @ApiOperation(value = "NFP 상세 조회", notes = "token id로 NFP의 상세정보를 조회한다.")
+    @ApiImplicitParam(name = "tokenId", value = "토큰 아이디", required = true)
     public ResponseEntity<NfpDetailDto> getNfpDetail(@PathVariable Long tokenId) throws Exception {
 
         UserPokemon userPokemon = pokedexService.getNfpDetail(tokenId);
