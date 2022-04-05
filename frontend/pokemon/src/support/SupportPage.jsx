1787c2435e401c40d1d8dd4de6b20c392dcb52d2
onys <thkim2017@gmail.com>
2022-04-05 16:21:42 +0900

    fix: 🐛 고객센터 취소 버튼 눌러도 메인으로 가게 수정

--- frontend/pokemon/src/support/SupportPage.jsx
+++ frontend/pokemon/src/support/SupportPage.jsx
@@ -87,7 +87,7 @@ export default function SupportPage() {
       </div>
       
       <div className="buttons">
-        <button className="cancel-button"><Link to="/main">취소</Link></button>
+        <Link to="/main"><button className="cancel-button">취소</button></Link>
         <button className="submit-button" onClick={onClickSubmitSupport}>제출</button>
       </div>
       { isLoading &&
