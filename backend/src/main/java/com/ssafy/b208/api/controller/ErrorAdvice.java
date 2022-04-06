dcb9e6d983247a4a7aa8bad091414c580766a07a
DESKTOP-KVCQHCD\SSAFY <shaian2@naver.com>
2022-04-06 15:22:18 +0900

    feat: getNfpList 예외처리

--- backend/src/main/java/com/ssafy/b208/api/controller/ErrorAdvice.java
+++ backend/src/main/java/com/ssafy/b208/api/controller/ErrorAdvice.java
@@ -4,6 +4,7 @@ import com.ssafy.b208.api.dto.response.BaseResponseBody;
 import com.ssafy.b208.api.exception.LackMoneyException;
 import com.ssafy.b208.api.exception.ExistIdException;
 import com.ssafy.b208.api.exception.NonExistentPokedexIdException;
+import com.ssafy.b208.api.exception.WrongPublicKeyException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -31,4 +32,11 @@ public class ErrorAdvice {
 
         return ResponseEntity.status(400).body(BaseResponseBody.of(400, "존재하지 않는 포켓몬 아이디입니다."));
     }
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(WrongPublicKeyException.class)
+    public ResponseEntity<BaseResponseBody> WrongPublicKey() {
+
+        return ResponseEntity.status(400).body(BaseResponseBody.of(400, "잘못된 공개키입니다."));
+    }
+
 }
--- backend/src/main/java/com/ssafy/b208/api/service/PokedexServiceImpl.java
+++ backend/src/main/java/com/ssafy/b208/api/service/PokedexServiceImpl.java
@@ -10,6 +10,7 @@ import com.ssafy.b208.api.db.repository.UserRepository;
 import com.ssafy.b208.api.dto.request.UserRequestDto;
 import com.ssafy.b208.api.dto.response.*;
 import com.ssafy.b208.api.exception.NonExistentPokedexIdException;
+import com.ssafy.b208.api.exception.WrongPublicKeyException;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -20,6 +21,7 @@ import javax.swing.text.html.Option;
 import javax.validation.constraints.Null;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.NoSuchElementException;
 import java.util.Optional;
 
 @Slf4j
@@ -71,28 +73,32 @@ public class PokedexServiceImpl implements PokedexService {
 
     @Override
     public NfpListDto getNfpList(String publicKey, Long pokedexId) {
-        // public key로 userId 찾기
-        User user = Optional.ofNullable(userRepository.findOptionalByPublicKey(publicKey).get())
-                .orElseGet(() -> User.builder().build());
-        Long userId = user.getId();
+        try {
+            // public key로 userId 찾기
+            User user = Optional.ofNullable(userRepository.findOptionalByPublicKey(publicKey).get())
+                    .orElseGet(() -> User.builder().build());
+            Long userId = user.getId();
 
-        List<UserPokemon> userPokemons = Optional.ofNullable(userPokemonRepository.findNfpList(userId, pokedexId).get())
-                .orElseGet(() -> new ArrayList<>());
-        List<NfpDetailDto> nfpList= new ArrayList<>();
-        for (UserPokemon userPokemon : userPokemons) {
-            NfpDetailDto nfpDetailDto = NfpDetailDto.builder()
-                    .tokenId(userPokemon.getTokenId())
-                    .pokedexId(userPokemon.getPokemon().getId())
-                    .ipfsMetaUri(userPokemon.getIpfsMetaUri())
-                    .ipfsImageUri(userPokemon.getIpfsImageUri())
-                    .grade(userPokemon.getGrade())
+            List<UserPokemon> userPokemons = Optional.ofNullable(userPokemonRepository.findNfpList(userId, pokedexId).get())
+                    .orElseGet(() -> new ArrayList<>());
+            List<NfpDetailDto> nfpList= new ArrayList<>();
+            for (UserPokemon userPokemon : userPokemons) {
+                NfpDetailDto nfpDetailDto = NfpDetailDto.builder()
+                        .tokenId(userPokemon.getTokenId())
+                        .pokedexId(userPokemon.getPokemon().getId())
+                        .ipfsMetaUri(userPokemon.getIpfsMetaUri())
+                        .ipfsImageUri(userPokemon.getIpfsImageUri())
+                        .grade(userPokemon.getGrade())
+                        .build();
+                nfpList.add(nfpDetailDto);
+            }
+            NfpListDto nfpListDto = NfpListDto.builder()
+                    .nfpList(nfpList)
                     .build();
-            nfpList.add(nfpDetailDto);
+            return nfpListDto;
+        } catch (NoSuchElementException e) {
+            throw new WrongPublicKeyException(publicKey);
         }
-        NfpListDto nfpListDto = NfpListDto.builder()
-                .nfpList(nfpList)
-                .build();
-        return nfpListDto;
     }
 
     @Override
