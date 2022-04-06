ee5719bf54daa4f705f9dba57473838e785038a1
onys <thkim2017@gmail.com>
2022-04-06 23:05:31 +0900

    feat: 🎸 포켓몬 상세정보 만드는 중

--- frontend/pokemon/src/pokedex/NfpItem.jsx
+++ frontend/pokemon/src/pokedex/NfpItem.jsx
@@ -1,13 +1,13 @@
 import React from "react";
 
-export default function NfpItem({ pokemonNum, pokemonName, nfpImgPath, grade }) {
+export default function NfpItem({ onClickToggleModalVisibility, pokemonNum, pokemonName, nfpImgPath, grade }) {
   const backgroundStyle ={
     backgroundImage: `url(/images/backgroundImg/${grade}.png)`
   };
   console.log(backgroundStyle);
   
   return (
-    <div className="pokemon-item" style={ backgroundStyle }>
+    <div className="pokemon-item" onClick={onClickToggleModalVisibility} style={ backgroundStyle }>
       <div className="pokemon-title">
         <div className="pokemon-num">
           <p>{pokemonNum}</p>
